edcf78a808d404bcf0de49e4a78f1d40093f2409
Bryan Dixon <javawolfpack@gmail.com>
2018-02-13 17:33:46 -0800

    Getting Started w/ Templates

--- code/myapp/urls.py
+++ code/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('page<int:page_num>/',views.page),
 ]
--- code/myapp/views.py
+++ code/myapp/views.py
@@ -3,4 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    #return HttpResponse("Hello World")
+    return render(request, 'index.html')
+
+def page(request, page_num):
+    if page_num>=1:
+        example_list=[]
+        for i in range(page_num):
+            example_list+=[i+1]
+
+    else:
+        example_list=None
+    #return HttpResponse("Hello World")
+    context={
+        "page_template":page_num,
+        "example_list":example_list
+        }
+    return render(request, 'page.html',context)
